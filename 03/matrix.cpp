e246ba340a513fa326f6d09ce8c9af5176cf5532
Dmitry <malugin99@mail.ru>
2019-03-29 23:15:28 +0300

    Third done

---
+++ 03/matrix.cpp
@@ -0,0 +1,77 @@
+#include "matrix.h"
+
+int& Matrix::Row::operator[](int index) {
+	if (index < 0 || index >= columns)
+		throw std::out_of_range("Wrong index");
+
+	return row_ptr[index];
+}
+
+Matrix::Matrix(size_t rows_count, size_t columns_count) {
+	if (rows_count < 0 || columns_count < 0)
+		throw std::out_of_range("Can't create matrix with size 0");
+
+	rows = rows_count;
+	columns = columns_count;
+	matrix = (int *) calloc(rows * columns, sizeof(int));
+}
+
+Matrix::Matrix(const Matrix &m) {
+	rows = m.rows;
+	columns = m.columns;
+	matrix = (int *) calloc(rows * columns, sizeof(int));
+
+	for (size_t i = 0; i < rows; ++i)
+		for (size_t j = 0; j < columns; ++j)
+			*(matrix + i * columns + j) = m[i][j];
+}
+
+size_t Matrix::getRows() const {
+	return rows;
+}
+
+size_t Matrix::getColumns() const {
+	return columns;
+}
+
+bool Matrix::operator==(const Matrix& other) const {
+	if (this == &other)
+		return true;
+
+	if (rows != other.getRows() || columns != other.getColumns())
+		return false;
+
+	for (size_t i = 0; i < rows; ++i)
+		for (size_t j = 0; j < columns; ++j)
+			if (matrix[i * columns + j] != other[i][j])
+			return false;
+
+	return true;
+}
+
+bool Matrix::operator!=(const Matrix& other) const {
+	return !(*this == other);
+}
+
+Matrix Matrix::operator*=(const int& a) {
+	for (size_t i = 0; i < rows; ++i)
+		for (size_t j = 0; j < columns; ++j)
+			(*this)[i][j] *= a;
+	Matrix tmp = *this;
+
+	return tmp;
+}
+
+Matrix::Row Matrix::operator[](int index) {
+	if (index < 0 || index >= rows)
+		throw std::out_of_range("Wrong index");
+
+	return Row(matrix + index * columns, columns);
+}
+
+Matrix::Row Matrix::operator[](int index) const {
+	if (index < 0 || index >= rows)
+		throw std::out_of_range("Wrong index");
+
+	return Row(matrix + index * columns, columns);
+}
--- 03/matrix.h
+++ 03/matrix.h
@@ -1,71 +1,40 @@
 #ifndef _MATRIX_
 #define _MATRIX_
 
+#include <cstdlib>
+#include <stdexcept>
+
 class Matrix {
 private:
 	size_t rows;
 	size_t columns;
-	int* matrix;
+	int *matrix;
+
 public:
-	Matrix (int rows_count = 1, int cols_count = 1) {
-		if (rows_count <= 0 || cols_count <= 0) {
-			throw std::out_of_range("");
-		}
-		rows = rows_count;
-		columns = cols_count;
-		matrix = (int*) calloc(rows * columns, sizeof(int));
-	}
-	~Matrix () { delete[] matrix; }
-
-	class Vector {
+	class Row {
 	private:
-		int* row;
-		size_t length;
+		size_t columns;
+		int *row_ptr;
 	public:
-		Vector(size_t row_num, size_t rows_count) {
-			length = rows_count;
-			row = *(matrix + row_num);
-		}
-		int& operator[](int x) {
-			if (x > 0 && x < length)
-				throw std::out_of_range("");
+		Row(int *row, size_t cols)
+				: columns(cols), row_ptr(row) {}
 
-			return *(row + x);
-		}
+		int &operator[](int index);
 	};
-	Vector& operator[](int ind) {
-		if (ind < 0 || ind >= rows)
-			throw std::out_of_range("");
-		return Vector(ind, rows, matrix);
-	}
-	bool operator==(const Matrix& other) const {
-		if (this == &other)
-            return true;
 
-        for (size_t i = 0; i < rows; ++i)
-			for (size_t j = 0; j < columns; ++j)
-            	if (*(*(matrix + i) + j) != other[i][j])
-                	return false;
+	Matrix(size_t rows_count = 1, size_t columns_count = 1);
+	Matrix(const Matrix &m);
+	~Matrix() { free(matrix); }
 
-        return true;
-	}
-	bool operator!=(const Matrix& other) const {
-		return !(*this == other);
-	}
-	Matrix& operator*=(int a) {
-		for (size_t i = 0; i < rows; ++i) {
-			for (size_t j = 0; j < columns; j++) {
-				*(matrix + i * columns + j) *= a;
-			}
-		}
-		return this;
-	}
-	size_t getRows() const {
-		return rows;
-	}
-	size_t getColumns() const {
-		return rows;
-	}
+	size_t getRows() const;
+	size_t getColumns() const;
+
+	bool operator==(const Matrix& other) const;
+	bool operator!=(const Matrix& other) const;
+	Matrix operator*=(const int& a);
+
+	Row operator[](int index);
+	Row operator[](int index) const;
 };
 
 #endif
